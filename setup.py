6847550e7717102bdf632a3e91dbfbb61ddc59b1
mjlally <m.j.lally@outlook.com>
2022-07-11 23:49:44 +0100

    pysaver Initial commit.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'pysaver',
+    version = '0.0',
+    py_modules = ['pysaver'],
+    author = 'M J Lally',
+    author_email = 'm.j.lally@outlook.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    license = 'MIT',
+    keywords = 'pysaver',
+)
